fa990848c8ca303394d14bf5fc0be2c071321c19
User <User@LAPTOP-9UAQP9SP>
2019-06-15 16:29:04 -0400

    Creating Repository

---
+++ src/simple/java.java
@@ -0,0 +1,19 @@
+/*
+ * 
+ * Riya Sharma
+ * Student ID: 991541511
+ * PROG24178 Object Oriented Programming 2- JAVA
+ */
+package simple;
+
+/**
+ *
+ * @author User
+ */
+public class java {
+    public static void main(String args[]){
+        System.out.println("Hello Java");
+        System.out.println("Git Practice in progress");
+        
+                 }
+}
